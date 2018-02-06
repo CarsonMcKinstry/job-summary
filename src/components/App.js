99f9f5088f085ef016b27972053918d701e7040a
Carson McKinstry <carson.mckinstry@gmail.com>
2018-02-06 16:06:34 -0600

    Updated to use scss and to use google fonts

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.scss';
 
 class App extends Component {
@@ -7,8 +6,8 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
+          <i class="material-icons">face</i>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
---
+++ src/images/unsplash.md
@@ -0,0 +1,2 @@
+face-1.jpg by [Ryan Holloway](https://unsplash.com/photos/oJUTLMxMsgE?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText) on Unsplash
+face-2 by [Tanja Heffner](https://unsplash.com/photos/OfOODQD-sy0?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText) on Unsplash
